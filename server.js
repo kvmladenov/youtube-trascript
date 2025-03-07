22dee1d05cf3b04f4d145093bda41f1e8189637f
Krasimir Mladenov <krasimir.mladenov@gmail.com>
2025-03-07 20:45:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const path = require('path');
 const app = express();
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html at the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/get-transcript', async (req, res) => {
     try {
